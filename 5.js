5fff5eb4039c8e98b321d0d99d614b524e7575c2
yeonghoon123 <haa3163314@naver.com>
2021-01-24 18:16:32 +0900

    asd

--- 5.js
+++ 5.js
@@ -1,2 +1,10 @@
 var tf = require("@tensorflow/tfjs")
-console.log(tf)             
+console.log(tf)
+
+/*
+let X =  tf.input({ shape : [1]});
+            let Y = tf.layers.dense({ units: 1}).apply(X);
+            let model = tf.model({inputs : X, outputs : Y});
+            let compileParam = { optimizer : tf.train.adam(), loss : tf.losses.meanSquaredError}
+            model.compile(compileParam);
+            */
